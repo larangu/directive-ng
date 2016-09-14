9f8b3d458598b3c56390f394d5865cab9b37442d
Filip Koblański <filip.koblanski@gmail.com>
2016-09-14 23:42:46 +0200

    Added base directives to generator.

--- src/DirectiveNgServiceProvider.php
+++ src/DirectiveNgServiceProvider.php
@@ -1,9 +1,6 @@
 <?php namespace Larangu\DirectiveNg;
 
 use Illuminate\Support\ServiceProvider;
-use Larangu\DirectiveNg\Directive\Generator;
-use Larangu\FormNg\Contracts\FormBuilder;
-use Larangu\FormNg\Form\BuilderNg;
 
 class DirectiveNgServiceProvider extends ServiceProvider
 {
@@ -30,6 +27,8 @@ class DirectiveNgServiceProvider extends ServiceProvider
         ], 'views');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'directive-ng');
+
+        $this->registerBladeDirectives();
     }
 
     /**
@@ -41,8 +40,72 @@ class DirectiveNgServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'directive-ng');
 
-        $this->app->singleton('directiveGeneratorNg', function () {
+        $this->app->singleton('directiveNg', function () {
             return $this->app->make(Generator::class);
+        });
+    }
+
+    /**
+     * REgistering blade directives for Angular generator.
+     */
+    protected function registerBladeDirectives()
+    {
+        \Blade::directive('ngAttr', function ($key, $expression) {
+            return app('directiveNg')->attr($key, $expression);
+        });
+
+        \Blade::directive('ngEvent', function ($event, $expression) {
+            return app('directiveNg')->event($event, $expression);
+        });
+
+        \Blade::directive('ngTemplate', function ($template, $expression = null) {
+            return app('directiveNg')->template($template, $expression);
+        });
+
+        \Blade::directive('ngIf', function ($condition) {
+            return app('directiveNg')->ngIf($condition);
+        });
+
+        \Blade::directive('ngFor', function ($expression, $singel = null, $index = null, $trackBy = null) {
+            return app('directiveNg')->ngFor($expression, $singel, $index, $trackBy);
+        });
+
+        \Blade::directive('ngClass', function ($expression) {
+            return app('directiveNg')->ngClass($expression);
+        });
+
+        \Blade::directive('ngStyle', function ($expression) {
+            return app('directiveNg')->ngStyle($expression);
+        });
+
+        \Blade::directive('ngLink', function ($link, $fullExpresion = false) {
+            if ($fullExpresion) {
+                return app('directiveNg')->ngRouterLink($link);
+            }
+            return app('directiveNg')->ngLink($link);
+        });
+
+        \Blade::directive('ngSwitch', function ($expression) {
+            return app('directiveNg')->ngSwitch($expression);
+        });
+
+        \Blade::directive('ngSwitchCase', function ($expression = null) {
+            if (!$expression) {
+                return app('directiveNg')->ngSwitchDefault();
+            }
+            return app('directiveNg')->ngSwitch($expression);
+        });
+
+        \Blade::directive('ngClick', function ($expression) {
+            return app('directiveNg')->click($expression);
+        });
+
+        \Blade::directive('ngSubmit', function ($expression) {
+            return app('directiveNg')->ngSubmit($expression);
+        });
+
+        \Blade::directive('nagModel', function ($expression) {
+            return app('directiveNg')->model($expression);
         });
     }
 }
---
+++ src/Generator.php
@@ -0,0 +1,105 @@
+<?php namespace Larangu\DirectiveNg;
+
+use Larangu\DirectiveNg\Contracts\DirectiveGenerator;
+
+class Generator implements DirectiveGenerator
+{
+    public function attr($key, $expression)
+    {
+        return "[{$key}]=\"{$expression}\"";
+    }
+
+    public function event($event, $expression)
+    {
+        return "({$event})=\"{$expression}\"";
+    }
+
+    public function template($template, $expression = null)
+    {
+        if (is_null($expression)) {
+            return "*{$template}";
+        }
+
+        return "*{$template}=\"{$expression}\"";
+    }
+
+    public function ngIf($condition)
+    {
+        return $this->template('ngIf', $condition);
+    }
+
+    public function ngFor($expression, $singel = null, $index = null, $trackBy = null)
+    {
+        if (!is_null($singel)) {
+            $expression = "let {$singel} of {$expression}";
+        }
+
+        if (!is_null($index)) {
+            $expression .= "; {$index}=index";
+        }
+
+        if (!is_null($trackBy)) {
+            $expression .= "; trackBy:{$trackBy}";
+        }
+
+        return $this->template('ngFor', $expression);
+    }
+
+    public function ngClass($expression)
+    {
+        if (is_array($expression)) {
+            $expression = str_replace("\"", '', json_encode($expression));
+        }
+
+        return $this->attr('ngClass', $expression);
+    }
+
+    public function ngStyle($expression)
+    {
+        if (is_array($expression)) {
+            $expression = str_replace("\"", '', json_encode($expression));
+        }
+
+        return $this->attr('ngStyle', $expression);
+    }
+
+    public function ngRouterLink($expression)
+    {
+        return $this->attr('routerLink', $expression);
+    }
+
+    public function ngLink($link)
+    {
+        return $this->attr('routerLink', "['{$link}']");
+    }
+
+    public function ngSwitch($expression)
+    {
+        return $this->attr('ngSwitch', $expression);
+    }
+
+    public function ngSwitchCase($value)
+    {
+        return $this->template('ngSwitchCase', "'{$value}'");
+    }
+
+    public function ngSwitchDefault()
+    {
+        return $this->template('ngSwitchDefault');
+    }
+
+    public function click($expression)
+    {
+        return $this->event('click', $expression);
+    }
+
+    public function ngSubmit($expression)
+    {
+        return $this->event('ngSubmit', $expression);
+    }
+
+    public function model($key)
+    {
+        return "[(ngModel)]=\"{$key}\"";
+    }
+}
